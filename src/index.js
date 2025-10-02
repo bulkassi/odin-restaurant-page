0dcc509a58943bd2ee1fcf938a71ce81dade757c
bulkassi <158156178+bulkassi@users.noreply.github.com>
2025-10-02 21:57:05 +0300

    Add functional click listeners for pages' rendering

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import "./style.css";
 import { renderAboutPage } from "./aboutPageController.js";
+import { renderMenuPage } from "./menuPageController.js";
 
 const ScreenController = (function (doc) {
   const homeBtn = doc.querySelector(".tabs .btn-home");
+  const menuBtn = doc.querySelector(".tabs .btn-menu");
   doc.querySelector("#content").replaceChildren();
-  homeBtn.addEventListener("click", renderAboutPage(doc));
+  homeBtn.addEventListener("click", renderAboutPage);
+  menuBtn.addEventListener("click", renderMenuPage);
+
+  renderAboutPage(doc);
 })(document);
---
+++ src/menuPageController.js
@@ -0,0 +1,85 @@
+import RestaurantImage from "./public/restaurant_default.svg";
+
+const contentContainer = document.querySelector("#content");
+
+const createContentBox = (doc) => {
+  const box = doc.createElement("div");
+  box.classList.add("content-box");
+
+  return box;
+};
+
+const createContent = (doc) => {
+  const createRestaurantNameBox = (doc) => {
+    const restaurantNameBox = createContentBox(doc);
+    restaurantNameBox.classList.add("flex-column");
+    restaurantNameBox.classList.add("align-center");
+
+    const restaurantName = doc.createElement("h1");
+    const emphasized = doc.createElement("em");
+    emphasized.append("The");
+    restaurantName.append(emphasized, " Snack Restaurant");
+
+    // const restaurantSvg = doc.createElement("img");
+    // restaurantSvg.src = RestaurantImage;
+
+    restaurantNameBox.appendChild(restaurantName);
+    // restaurantNameBox.appendChild(restaurantSvg);
+
+    return restaurantNameBox;
+  };
+
+  const createDescriptionBox = (doc) => {
+    const descriptionBox = createContentBox(doc);
+    const description = doc.createElement("p");
+    const descEm = doc.createElement("em");
+    descEm.append("bonus");
+    description.append(
+      "Our restaurant presents all known yet more flavourful snacks to try \
+          for everyone! The first guests of the day receive a ",
+      descEm,
+      "!"
+    );
+    descriptionBox.appendChild(description);
+
+    return descriptionBox;
+  };
+
+  const createScheduleBox = (doc) => {
+    const scheduleBox = createContentBox(doc);
+    scheduleBox.classList.add("flex-column");
+    scheduleBox.classList.add("align-center");
+
+    const scheduleHeader = doc.createElement("h2");
+    scheduleHeader.append("Hours");
+
+    const scheduleMonFri = doc.createElement("p");
+    scheduleMonFri.append("Mon-Fri: 10am - 10pm");
+
+    const scheduleSat = doc.createElement("p");
+    scheduleSat.append("Sat: 10am - 8pm");
+
+    const scheduleSun = doc.createElement("p");
+    const emphasized = doc.createElement("em");
+    emphasized.append("Day off");
+    scheduleSun.append("Sun: ", emphasized);
+
+    scheduleBox.appendChild(scheduleHeader);
+    scheduleBox.appendChild(scheduleMonFri);
+    scheduleBox.appendChild(scheduleSat);
+    scheduleBox.appendChild(scheduleSun);
+
+    return scheduleBox;
+  };
+
+  contentContainer.appendChild(createRestaurantNameBox(doc));
+  contentContainer.appendChild(createDescriptionBox(doc));
+  contentContainer.appendChild(createScheduleBox(doc));
+};
+
+const renderMenuPage = (doc) => {
+  contentContainer.replaceChildren();
+  createContent(document);
+};
+
+export { renderMenuPage };
